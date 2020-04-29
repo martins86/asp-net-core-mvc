2a24f202a5a41136e46d397a96eecb05eefc8257
thiagodf <thiagodf@ciandt.com>
2020-04-29 16:56:47 -0300

    Better editing the commands

--- clear_docker_all.sh
+++ clear_docker_all.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
-
+echo 'Show all'
+docker image ls
+echo ''
+docker container ls
+echo ''
+docker volume ls
+echo ''
+docker network ls
+echo ''
+echo ''
+echo 'Stop all'
+all=$(docker container ls -a -q)
+docker container stop $all
+echo ''
 docker system prune --all --force --volumes
