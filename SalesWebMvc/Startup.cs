bc29e71ec7df060d25a311e65d0761fb246ea15c
Thiago Martins <martins86@gmail.com>
2020-05-06 09:53:56 -0300

    Instantiating department service

--- SalesWebMvc/Startup.cs
+++ SalesWebMvc/Startup.cs
@@ -38,10 +38,11 @@ namespace SalesWebMvc
 
             services.AddScoped<SeedingService>();
             services.AddScoped<SellerService>();
+            services.AddScoped<DepartmentService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, SeedingService seedingService)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, SeedingService seedingService, DepartmentService departmentService)
         {
             if (env.IsDevelopment())
             {
