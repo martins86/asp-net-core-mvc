984607b08b0f9e5acf8a0e5a6ebffb40dd272118
Thiago Martins <martins86@gmail.com>
2020-05-06 09:51:25 -0300

    Include department

--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -7,22 +7,22 @@ namespace SalesWebMvc.Services
 {
     public class SellerService
     {
-        private readonly SalesWebMvcContext _contest;
+        private readonly SalesWebMvcContext _context;
 
         public SellerService(SalesWebMvcContext context)
         {
-            _contest = context;
+            _context = context;
         }
 
         public List<Seller> FindAll()
         {
-            return _contest.Seller.ToList();
+            return _context.Seller.ToList();
         }
 
         public void Insert(Seller obj)
         {
-            _contest.Add(obj);
-            _contest.SaveChanges();
+            _context.Add(obj);
+            _context.SaveChanges();
         }
     }
 }
